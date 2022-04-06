e87cc483ecfbfffb5783200cc6fb5dc02f37c680
Jiamin <jianminmu@microsoft.com>
2022-04-07 00:08:16 +0800

    add cloud configuration

--- threatfox-ioc/main.go
+++ threatfox-ioc/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/securityinsights/armsecurityinsights"
 	"github.com/cosos/firefly/cloudlib/azurecloud"
@@ -15,7 +18,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
-	tiClient := armsecurityinsights.NewThreatIntelligenceIndicatorClient(os.Getenv("Subscription"), creds, nil)
+	tiClient := armsecurityinsights.NewThreatIntelligenceIndicatorClient(os.Getenv("Subscription"), creds, &arm.ClientOptions{policy.ClientOptions{Cloud: cloud.AzurePublicCloud}})
 	if azurecloud.CheckIndicator(tiClient, context.Background(), "Core", "win.emotet") {
 		log.Println("found it")
 	}
