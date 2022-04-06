f0cfd39811f9eeeaa7f729c8bdb817d80721425c
Jiamin <jianminmu@microsoft.com>
2022-04-07 00:25:45 +0800

    change client option

--- threatfox-ioc/main.go
+++ threatfox-ioc/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/cloud"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/securityinsights/armsecurityinsights"
 	"github.com/cosos/firefly/cloudlib/azurecloud"
@@ -15,7 +18,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
-	tiClient := armsecurityinsights.NewThreatIntelligenceIndicatorClient(os.Getenv("Subscription"), creds, nil)
+	option := arm.ClientOptions{policy.ClientOptions{Cloud: cloud.AzurePublicCloud}}
+	tiClient := armsecurityinsights.NewThreatIntelligenceIndicatorClient(os.Getenv("Subscription"), creds, &option)
 	if azurecloud.CheckIndicator(tiClient, context.Background(), "Core", "win.emotet") {
 		log.Println("found it")
 	}
